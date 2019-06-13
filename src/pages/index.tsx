6aa95d198a647fae730eec8721b192378549444e
jawad <j.taibi59@gmail.com>
2019-06-13 16:15:09 +0200

    modif du texte

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,62 @@
 import React from 'react'
-import Container from '@material-ui/core/Container'
+import PropTypes from 'prop-types'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
 import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import MuiLink from '@material-ui/core/Link'
-import ProTip from '../components/ProTip'
-import Link from '../components/Link'
+import Toolbar from '@material-ui/core/Toolbar'
 
-const MadeWithLove = () => (
-  <Typography variant="body2" color="textSecondary" align="center">
-    {'Built with love by the '}
-    <MuiLink color="inherit" href="https://material-ui.com/">
-      Material-UI
-    </MuiLink>
-    {' team.'}
-  </Typography>
+interface TabContainerProps {
+  children?: React.ReactNode
+}
+
+function TabContainer(props: TabContainerProps) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  )
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      backgroundColor: theme.palette.background.paper,
+    },
+  }),
 )
 
-const App = () => (
-  <Container maxWidth="sm">
-    <Box my={4}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Gatsby v4-beta example
-      </Typography>
-      <Link to="/about" color="secondary">
-        Go to the about page
-      </Link>
-      <ProTip />
-      <MadeWithLove />
-    </Box>
-  </Container>
-)
+export default function SimpleTabs() {
+  const classes = useStyles()
+  const [value, setValue] = React.useState(0)
 
-export default App
+  function handleChange(event: React.ChangeEvent<{}>, newValue: number) {
+    setValue(newValue)
+  }
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Isis.Care
+          </Typography>
+        </Toolbar>
+        <Tabs value={value} onChange={handleChange}>
+          <Tab label="Accueil" />
+          <Tab label="Solution" />
+          <Tab label="Contact" />
+        </Tabs>
+      </AppBar>
+      {value === 0 && <TabContainer>Bienvenu(e) sur Isis.Care</TabContainer>}
+      {value === 1 && <TabContainer>Produit</TabContainer>}
+      {value === 2 && <TabContainer>Contacter-nous !</TabContainer>}
+    </div>
+  )
+}
